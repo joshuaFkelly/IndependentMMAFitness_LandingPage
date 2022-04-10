9931e24a1f34aa042faf7eebcbd5e8563061709d
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-04-10 17:17:16 -0400

    HERO SECTION DONE BITCHHHHHH

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -7,25 +7,23 @@ export const HeroSection = () => {
         // Image as background 
 
 
-        <section className="bg-mobile-gloves bg-cover bg-top h-screen w-screen bg-no-repeat flex flex-col p-1 tablet:bg-desktop-gloves xl:bg-cover">
+        <section className="bg-mobile-gloves bg-cover bg-top h-screen w-screen bg-no-repeat flex flex-col justify-evenly p-1 tablet:bg-desktop-gloves xl:bg-cover">
 
-            <article className="text-white mt-10">
 
-                <div className="text-right border-r-4 border-red-600">
-                    <h1 className="text-2xl pb-16 font-light mr-1">MIXED <span className={sport}>MARTIAL ARTS</span></h1>
-                    <h1 className="text-2xl pb-16 font-light mr-1">BRAZILLIAN <span className={sport}>JIU-JITSU</span></h1>
-                    <h1 className="text-2xl pb-16 font-light mr-1">CARDIO <span className={sport}>KICKBOXING</span></h1>
-                    <h1 className="text-2xl font-light mr-1">MUAY <span className={sport}>THAI</span></h1>
-                </div>
+            <article className="text-white text-right border-r-4 border-red-600">
+                <h1 className="text-5xl py-2 font-light mr-1">MIXED<br /><span className={sport}>MARTIAL ARTS</span></h1>
+                <h1 className="text-5xl py-2 font-light mr-1">BRAZILLIAN<br /><span className={sport}>JIU-JITSU</span></h1>
+                <h1 className="text-5xl py-2 font-light mr-1">CARDIO<br /><span className={sport}>KICKBOXING</span></h1>
+                <h1 className="text-5xl py-2 font-light mr-1">MUAY<br /><span className={sport}>THAI</span></h1>
             </article>
 
-            <article className="text-white m-auto text-center">
 
-                <p className="text-lg italic mb-2">"If you want to go <span className="text-xl text-red-600">fast,</span> go alone. If you want to go <span className="text-xl text-red-600">far,</span> go together."</p>
-                <CTAButton style={"text-white font-semibold text-lg rounded p-2 bg-red-700"} btnName="Start Your Journey" />
+            <article className="text-white text-center">
+
+                <p className="text-2xl italic mb-4">"If you want to go <span className="text-3xl text-red-600">fast,</span> go alone. If you want to go <span className="text-xl text-red-600">far,</span> go together."</p>
+                <CTAButton style={"text-white font-semibold text-xl rounded p-3 bg-red-700"} btnName="Start Your Journey" />
 
             </article >
-
         </section >
 
 
