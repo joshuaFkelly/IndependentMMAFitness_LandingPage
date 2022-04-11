750ae5ece704f4bc458ae14648db5506b2fb1e1c
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-04-11 14:03:04 -0400

    some revisions

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -3,8 +3,6 @@ import { CTAButton } from "../CallToAction/Button";
 export const HeroSection = () => {
     const sport = "text-red-600 font-bold"
     return (
-        // Image as background 
-
 
         <section className="bg-mobile-gloves bg-cover bg-top bg-no-repeat flex flex-col gap-y-8 py-2 tablet:bg-desktop-gloves lg:bg-contain">
 
@@ -18,11 +16,10 @@ export const HeroSection = () => {
 
 
             <article className="text-white text-center">
-
-                <p className="text-xl italic mb-2">"If you want to go <span className="text-2xl text-red-600">fast,</span> go alone. If you want to go <span className="text-2xl text-red-600">far,</span> go together."</p>
+                <p className="text-xl italic mb-2">" If you want to go <span className="text-2xl text-red-600">fast,</span> go alone. If you want to go <span className="text-2xl text-red-600">far,</span> go together. "</p>
                 <CTAButton style={"text-white font-semibold text-xl rounded p-2 bg-red-700"} btnName="Start Your Journey" />
-
             </article >
+
         </section >
 
 
