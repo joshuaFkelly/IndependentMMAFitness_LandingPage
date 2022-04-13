bdd7db7de2e73619b4bf5bf822d734def9ccc393
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-04-13 15:16:00 -0400

    gettin there

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -9,16 +9,18 @@ export const Video = () => {
             <article>
 
                 <h1 className="text-4xl font-light">PHILOSOPHY</h1>
-                <ul className="flex flex-col gap-5 text-xl text-white mt-5">
-                    <li>Virtue #1</li>
-                    <li>Virtue #2</li>
-                    <li>Virtue #3</li>
-                    <li>Virtue #4</li>
+                <ul className="flex flex-col gap-5 text-2xl  font-bold mt-5">
+                    <li>RESPECT</li>
+                    <li>CONFIDENCE</li>
+                    <li>HEALTH</li>
+                    <li>FAMILY</li>
                 </ul>
             </article>
 
             <article className=" w-full py-5 " >
-                <iframe className="m-auto h-96 shadow-2xl shadow-gray-500" src="https://www.youtube-nocookie.com/embed/XppxZZAojkM" title="Team Independent BJJ Commercial" allowFullScreen></iframe>
+                <h1 className="text-4xl">Meet Our Coach's!</h1>
+                <p className="text-sm">Watch until the end for wise words from our boxing instructor, <span className="text-red-600 text-lg">Coach James!</span> </p>
+                <iframe className="m-auto h-96 shadow-2xl shadow-gray-500" src="https://www.youtube.com/embed/QnHOhwrqKfg" title="Team Independent BJJ Commercial" allowFullScreen></iframe>
             </article >
 
         </section >
